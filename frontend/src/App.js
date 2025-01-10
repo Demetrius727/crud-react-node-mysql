2878d19029095afe02c73602b69526fba4d1371f
Demetrius Carregosa <demetriuscarregosa@gmail.com>
2025-01-10 12:44:14 -0300

    Starting FormPart component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
 import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import FormPart from './components/FormPart.js';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
-  return
-  (
+  return (
   <>
-    
+    <Container>
+      <Title>USUÁRIOS</Title>
+      <FormPart />
+    </Container>
+    <ToastContainer autoClose={3000}/>
+    <GlobalStyle />
   </>
   );
 }
